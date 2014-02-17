4b1ad002db799215def2350d216be2089ed935d7
sahar <sahar@sahar>
2014-02-17 12:22:48 +0100

    new commit by sahar

--- src/tunipharma/gui/inscriptionPharmacien.java
+++ src/tunipharma/gui/inscriptionPharmacien.java
@@ -8,12 +8,12 @@ package tunipharma.gui;
  *
  * @author sahar
  */
-public class connexionPharmacien extends javax.swing.JFrame {
+public class inscriptionPharmacien extends javax.swing.JFrame {
 
     /**
-     * Creates new form connexionPharmacien
+     * Creates new form inscriptionPharmacien
      */
-    public connexionPharmacien() {
+    public inscriptionPharmacien() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class connexionPharmacien extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(connexionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(inscriptionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(connexionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(inscriptionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(connexionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(inscriptionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(connexionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(inscriptionPharmacien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new connexionPharmacien().setVisible(true);
+                new inscriptionPharmacien().setVisible(true);
             }
         });
     }

7a18988672d3486a8d1a31d3a7c4703de97cf2f7
wael.boumaiza <wael.boumaiza@waelboumaiza>
2014-02-17 08:19:07 +0100

    new commit by wael boumaiza

---
+++ src/tunipharma/util/MyConnection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tuniPharma.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author wael boumaiza
+ */
+public class MyConnection {
+
+   private static final  String driver = "com.mysql.jdbc.Driver";
+   private static final  String url = "jdbc:mysql://localhost:3306/tuniPharma";
+   private static final  String login = "root";
+   private static final  String pwd = "";
+   private static Connection con;
+
+
+   private MyConnection(){
+
+   }
+
+   public Connection etablirConnection(){
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,login,pwd);
+            System.out.println("Connexion établie");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erreur de chargement de driver"+ex.getMessage());
+        } catch (SQLException ex){
+            System.out.println("probleme d'etablissement de connection"+ex.getMessage());
+        }
+
+        return con;
+    }
+   public static Connection getInstance(){
+       if (con==null){
+           new MyConnection().etablirConnection();
+       }
+   return con;
+   }
+}
